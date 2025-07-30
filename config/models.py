7cf0ec1e9e22bbf8fbe24132fdea6d925308e82d
subashshakya <subashshakya12@gmail.com>
2025-07-30 06:20:06 +0545

    [UPDATE]: add meta class declaration

--- config/models.py
+++ config/models.py
@@ -2,11 +2,18 @@ import uuid
 
 from django.db import models
 
+
 class Timestamp(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField()
     deleted_at = models.DateTimeField()
 
+    class Meta:
+        abstract = True
+
 
 class BaseModel(Timestamp):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+
+    class Meta:
+        abstract = True
