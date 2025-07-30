9775b8c8b8df78ab21ead0f0919c68e07518bfa4
subashshakya <subashshakya12@gmail.com>
2025-07-30 06:19:45 +0545

    [FEAT]: add settings for channel layer and apps

--- ORGAN1C/settings.py
+++ ORGAN1C/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "config",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -68,7 +70,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "ORGAN1C.wsgi.application"
+# WSGI_APPLICATION = "ORGAN1C.wsgi.application"
+
+ASGI_APPLICATION = "ORGAN1C.asgi.application"
 
 
 # Database
@@ -122,3 +126,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
