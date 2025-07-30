9c540c4c7ca2e738bd836fd68ada47174fa3e923
subashshakya <subashshakya12@gmail.com>
2025-07-30 06:17:42 +0545

    [FEAT]: create asgi server for application and websocket configuration

--- ORGAN1C/asgi.py
+++ ORGAN1C/asgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from core.ws_urls import url_patterns
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ORGAN1C.settings')
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ORGAN1C.settings")
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(url_patterns))
+        ),
+    }
+)
